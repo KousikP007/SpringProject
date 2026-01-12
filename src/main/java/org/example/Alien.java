4220237c8f748143ea688c387e6479671339dc9b
KousikP007 <paul.kousik.a@gmail.com>
2026-01-12 09:18:37 +0530

    commmit 5: constructor injection

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -3,6 +3,15 @@ package org.example;
 public class Alien {
     private int age;
     private Laptop lap;
+    public Alien(){
+        System.out.println("in alien object");
+    }
+
+    public Alien(Laptop lap, int age) {
+        System.out.println("in constructor");
+        this.lap = lap;
+        this.age = age;
+    }
 
     public void coding(){
         System.out.println("coding....");
